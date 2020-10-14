4c6c0e91deeb1ca511e59e1cf45db067ff7feab1
Taylor Howell <66186140+taylorrhowell@users.noreply.github.com>
2020-10-13 19:38:04 -0700

    add  get all route

--- api/series.js
+++ api/series.js
@@ -3,6 +3,14 @@ const seriesRouter = express.Router();
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
-
+seriesRouter.get('/', (req, res, next) => {
+    db.all("SELECT * FROM Series", (err, series) => {
+        if (err) {
+            next(err);
+        } else {
+            res.status(200).json({series: series});
+        }
+    });
+});
 
 module.exports = seriesRouter;
