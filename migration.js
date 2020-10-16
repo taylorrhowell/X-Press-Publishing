e2b5e6e4eeb871c1b5eea6529bab4e7f26f09b3c
Taylor Howell <66186140+taylorrhowell@users.noreply.github.com>
2020-10-15 19:31:04 -0700

    create Issue table

--- migration.js
+++ migration.js
@@ -16,4 +16,15 @@ db.serialize(() => {
         description TEXT NOT NULL,
         PRIMARY KEY("id")
     );`);
+    db.run(`CREATE TABLE IF NOT EXISTS "Issue" (
+        "id" INTEGER NOT NULL,
+        "name" TEXT NOT NULL,
+        "issue_number" INTEGER NOT NULL,
+        "publication_date" TEXT NOT NULL,
+        "artist_id"	INTEGER NOT NULL,
+        "series_id"	INTEGER NOT NULL,
+        FOREIGN KEY("series_id") REFERENCES "Series"("id"),
+        PRIMARY KEY("id"),
+        FOREIGN KEY("artist_id") REFERENCES "Artist"("id")
+    );`)
 });
