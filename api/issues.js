573f1b4283d2a54be8cb7229c0f441e32e0cc1a3
Taylor Howell <66186140+taylorrhowell@users.noreply.github.com>
2020-10-15 20:23:35 -0700

    add get all issues route

--- api/issues.js
+++ api/issues.js
@@ -1,5 +1,20 @@
 const express = require('express');
 const issuesRouter = express.Router({mergeParams: true});
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+issuesRouter.get('/', (req, res, next) => {
+    const sql = "SELECT * FROM Issue WHERE Issue.series_id = $id;";
+    const values = {
+        $id: req.params.seriesId
+    }; 
+    db.all(sql, values, (err, issues) => {
+        if (err) {
+            next(err);
+        } else {
+            res.status(200).json({issues: issues});
+        }
+    });
+});
 
 module.exports = issuesRouter;
