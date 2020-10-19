70df94ab79aca980ec4d18d6c8e9e932ba264d46
Taylor Howell <66186140+taylorrhowell@users.noreply.github.com>
2020-10-19 09:19:40 -0700

    add issueId param and put route

--- api/issues.js
+++ api/issues.js
@@ -3,6 +3,18 @@ const issuesRouter = express.Router({mergeParams: true});
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+issuesRouter.param('issueId', (req, res, next, issueId) => {
+    db.get(`SELECT * FROM Issue WHERE Issue.id = ${issueId};`, (err, issue) =>{
+        if (err) {
+            next(err);
+        } else if (issue) {
+            next();
+        } else {
+            res.sendStatus(404);
+        };
+    });
+});
+
 issuesRouter.get('/', (req, res, next) => {
     const sql = "SELECT * FROM Issue WHERE Issue.series_id = $id;";
     const values = {
@@ -47,6 +59,43 @@ issuesRouter.post('/', (req, res, next) => {
                         next(err);
                     } else {
                         res.status(201).json({issue: issue});
+                    };
+                });
+            };
+        });
+    };
+});
+
+issuesRouter.put('/:issueId', (req, res, next) => {
+    const name = req.body.issue.name, issueNumber = req.body.issue.issueNumber, publicationDate = req.body.issue.publicationDate, artistId = req.body.issue.artistId;
+    if (!name || !issueNumber || !publicationDate || !artistId) {
+        return res.sendStatus(400);
+    };
+    const artistExists = db.get("SELECT * FROM Artist WHERE id = $artistId;", {artistId: artistId}, (err, artist) => {
+        if (err) {
+            return false;
+        } else {
+            return true;
+        };
+    });
+    if (!artistExists) {
+        return res.sendStatus(400);
+    } else {
+        db.run("UPDATE Issue SET name = $name, issue_number = $issueNumber,  publication_date = $publicationDate, artist_id = $artistId WHERE Issue.id = $issueId", {
+            $name: name,
+            $issueNumber: issueNumber,
+            $publicationDate: publicationDate,
+            $artistId: artistId,
+            $issueId: req.params.issueId
+        }, function(err) {
+            if (err) {
+                next(err);
+            } else {
+                db.get(`SELECT * FROM Issue WHERE Issue.id = ${req.params.issueId};`, (err, issue) => {
+                    if (err) {
+                        next(err);
+                    } else {
+                        res.status(200).json({issue: issue});
                     };
                 });
             };
